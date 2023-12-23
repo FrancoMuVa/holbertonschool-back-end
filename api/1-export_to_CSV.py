3bca0e313d55de8c3e692d8cc472a2971f205446
FrancoMuVa <muvafranco@gmail.com>
2023-12-23 14:38:57 +0000

    code changes

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -12,29 +12,7 @@ from sys import argv
 def employee_progress(employee_id):
     """ Show employee progres """
     URL = "https://jsonplaceholder.typicode.com"
-
     user = requests.get(f'{URL}/users/{employee_id}').json()
-    user_name = user["name"]
-
-    todo_list = requests.get(f'{URL}/users/{employee_id}/todos').json()
-
-    done_tasks, t_tasks = 0, 0
-    for task in todo_list:
-        if task["completed"] is True:
-            done_tasks = done_tasks + 1
-        t_tasks = t_tasks + 1
-
-    print(f"Employee {user_name} is done with tasks({done_tasks}/{t_tasks}):")
-    for task in todo_list:
-        if task["completed"] is True:
-            print(f"\t {task['title']}")
-
-
-def export_to_csv(employee_id):
-    """ export data in CSV format """
-    URL = "https://jsonplaceholder.typicode.com"
-    user = requests.get(f'{URL}/users/{employee_id}').json()
-
     todo_list = requests.get(f'{URL}/users/{employee_id}/todos').json()
 
     filename = f"{user['id']}.csv"
@@ -51,4 +29,4 @@ def export_to_csv(employee_id):
 
 
 if __name__ == "__main__":
-    export_to_csv(argv[1])
+    employee_progress(argv[1])
